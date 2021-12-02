7ae171ce478ea93cc887a4334ef4dab795117108
ILLIAKH <39228121+ILLIAKH@users.noreply.github.com>
2021-12-02 11:08:38 -0500

    back-changes

--- back/config/database.js
+++ back/config/database.js
@@ -1,7 +1,8 @@
 //create connection to database
 const mongoose = require("mongoose");
 
-const {MONGO_URI} = process.env;
+// const {MONGO_URI} = process.env;
+const {MONGO_URI} = "mongodb+srv://IKHOMENKO2:iphone3G20@senecaweb.mymvn.mongodb.net/PersonalProjects?retryWrites=true&w=majority";
 
 exports.connect = () => {
     mongoose.connect(MONGO_URI,{
